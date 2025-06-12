cb0afb817687422d82cdd0caa97c0905a6e5c33b
kyrylosheliah <kyrylosheliah@gmail.com>
2025-06-12 20:28:15 +0300

    hedgehog problem solution done; setup vs code debugging; initial powershell resource measuring script draft

---
+++ unsorted/hedgehog-population-coloring-problem/Problem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CommandLine;
+using Problem;
+
+var redOption = new Option<int>(
+    name: "--red",
+    description: "Number of red hedgehogs",
+    getDefaultValue: () => 0);
+redOption.AddAlias("-r");
+
+var greenOption = new Option<int>(
+    name: "--green",
+    description: "Number of green hedgehogs",
+    getDefaultValue: () => 0);
+greenOption.AddAlias("-g");
+
+var blueOption = new Option<int>(
+    name: "--blue",
+    description: "Number of blue hedgehogs",
+    getDefaultValue: () => 0);
+blueOption.AddAlias("-b");
+
+var targetOption = new Option<string>(
+    name: "--target",
+    description: "Target color: red, green, or blue");
+targetOption.IsRequired = true;
+targetOption.AddAlias("-t");
+
+var rootCommand = new RootCommand("Hedgehog color solver")
+{
+    redOption,
+    greenOption,
+    blueOption,
+    targetOption
+};
+rootCommand.SetHandler((red, green, blue, target) =>
+{
+    int[] population = [red, green, blue];
+    int result = Solver.Solve(population, target.ToLower());
+    //Console.WriteLine($"Result: {result}");
+    Console.WriteLine(result.ToString());
+}, redOption, greenOption, blueOption, targetOption);
+
+return await rootCommand.InvokeAsync(args);
---
+++ unsorted/hedgehog-population-coloring-problem/Problem/Solver.cs
@@ -0,0 +1,96 @@
+namespace Problem;
+
+public static class Solver
+{
+    public static int Solve(int[] population, string targetColor)
+    {
+        int targetCountIndex = targetColor switch
+        {
+            "red" => 0,
+            "green" => 1,
+            "blue" => 2,
+            _ => -1
+        };
+
+        if (targetCountIndex == -1)
+            return -1;
+
+        //var total = population[0] + population[1] + population[2];
+        var total = population.Sum();
+
+        // check if the problem is already solved
+        if (population[targetCountIndex] == total)
+            return 0;
+
+        // check if there's only one color present
+        var colorPresenceCount = 0;
+        for (int i = 0; i < 3; i++)
+            if (population[i] > 0)
+                ++colorPresenceCount;
+        if (colorPresenceCount == 1)
+            return -1;
+
+        // extract two other color indices
+        var others = new List<int>();
+        for (int i = 0; i < 3; ++i)
+            if (targetCountIndex != i)
+                others.Add(i);
+        var lesserCountIndex = others[0];
+        var greaterCountIndex = others[1];
+
+        // paired quantities = to be transfered to target color group
+        if (population[lesserCountIndex] == population[greaterCountIndex])
+            return population[lesserCountIndex];
+
+        if (population[lesserCountIndex] > population[greaterCountIndex])
+        { // swap
+            lesserCountIndex ^= greaterCountIndex;
+            greaterCountIndex ^= lesserCountIndex;
+            lesserCountIndex ^= greaterCountIndex;
+        }
+
+        // check invariance
+        // only other colors count difference of 3 makes the problem solvable
+        var lesserCount = population[lesserCountIndex];
+        var greaterCount = population[greaterCountIndex];
+        if ((greaterCount - lesserCount) % 3 != 0)
+            return -1;
+        var targetCount = population[targetCountIndex];
+
+        // main shaker transfer cycle
+        // example: [ 3 ; 3 ; 21 ]
+        // [ 9 ; 0 ; 18 ] // transfer
+        // equalization between 0 and 18 with lesser value growing twice
+        // as fast as greater one
+        // (9 - (18 / 3) >= 0 // check if can equalize
+        // [ 9 - (18 / 3) ; (18 / 3 * 2) ; 18 - (18 / 3) ] // equalize
+        var encountersNeeded = 0;
+        while (greaterCount > 0) {
+            // transfer
+            encountersNeeded += lesserCount;
+            greaterCount -= lesserCount;
+            targetCount += lesserCount * 2;
+            lesserCount = 0;
+            var third = greaterCount / 3;
+            if (targetCount >= third)
+            {
+                targetCount -= third;
+                // can equalize?
+                if (targetCount < 0)
+                    return -1;
+                encountersNeeded += third;
+                // equalize
+                greaterCount -= third;
+                lesserCount = third * 2; // was 0 after transfer
+            }
+            else // example: [9, 0, 8997]
+            { // equalize caps with target counter
+                encountersNeeded += targetCount;
+                lesserCount += targetCount * 2;
+                greaterCount -= targetCount;
+                targetCount = 0;
+            }
+        }
+        return encountersNeeded;
+    }
+}
